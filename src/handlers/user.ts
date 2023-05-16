25add734080d7ac5322ff12d53f1fc33b7754e7b
lucas neves <casluhc@gmail.com>
2023-05-16 19:23:57 -0300

    Refactoring

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,42 +1,4 @@
 import prisma from "../db";
-import { comparePasswords, createJWT, hashPassword } from "../modules/auth";
-
-export const signup = async (req, res) => {
-  const user = await prisma.user.create({
-    data: {
-      username: req.body.username,
-      password: await hashPassword(req.body.password),
-    },
-  });
-
-  if (!user) {
-    res.json({ error: "User not created" });
-    return;
-  }
-
-  res.status(201);
-  res.json({ message: "user created" });
-};
-
-export const signin = async (req, res) => {
-  const user = await prisma.user.findUnique({
-    where: {
-      username: req.body.username,
-    },
-  });
-
-  const isValid = await comparePasswords(req.body.password, user?.password);
-
-  if (!isValid) {
-    res.status(401);
-    res.json({ message: "Not Authorized" });
-    return;
-  }
-
-  const token = createJWT(user);
-
-  res.json({ token });
-};
 
 export const getUserInfo = async (req, res) => {
   const userId = req.user.id;
