11516694783cd42c47338389055a21b099c38f3c
lucas neves <casluhc@gmail.com>
2023-05-19 20:12:13 -0300

    Delete protein item

--- src/handlers/proteins.ts
+++ src/handlers/proteins.ts
@@ -1,25 +1,43 @@
-import prisma from "../db"
+import prisma from "../db";
 
 export const getProteinInfo = async (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
 
   const proteinAmount = await prisma.proteinAmount.findFirst({
     where: {
-      id, 
-      belongsToId: req.user.id
-    }
-  })
+      id,
+      belongsToId: req.user.id,
+    },
+  });
 
-  res.json({data: { proteinAmount, status: 200, user: req.user.username}})
-}
+  res.json({ data: { proteinAmount, status: 200, user: req.user.username } });
+};
 
 export const updateProteinAmount = async (req, res) => {
   const proteinAmount = await prisma.proteinAmount.create({
     data: {
       quantity: req.body.quantity,
-      belongsToId: req.user.id
-    }
-  })
+      belongsToId: req.user.id,
+    },
+  });
 
-  res.json({data: { proteinAmount, status: 200, user: req.user.username}})
-}
+  res.json({ data: { proteinAmount, status: 200, user: req.user.username } });
+};
+
+export const deleteProteinAmount = async (req, res) => {
+  const deleted = await prisma.proteinAmount.deleteMany({
+    where: {
+      id: req.params.id,
+      belongsToId: req.user.id,
+    },
+  });
+
+  if (!deleted.count) {
+    res.status(404)
+    res.json({message: "Couldn't delete protein"})
+  }
+
+  res.json({ data: deleted, message: "Protein item deleted" });
+};
+
+// 5e4d6bde-f368-4b63-9758-178102d14ef0
