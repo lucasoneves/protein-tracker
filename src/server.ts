f9fd9bba6c6a3d8513bed499c1c238bcc6ce93b9
lucas neves <casluhc@gmail.com>
2023-05-26 20:35:39 -0300

    WIP: Error on sending new password

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import router from './router';
 import { protect } from './modules/auth';
 import { signup, signin } from './handlers/auth';
-import { forgotPasswordHandler } from './handlers/password';
+import { forgotPasswordHandler, resetPasswordHandler, sendNewPassword } from './handlers/password';
 
 const app = express();
 
@@ -15,12 +15,12 @@ app.use('/api', protect, router);
 app.post('/signup', signup);
 app.post('/signin', signin)
 
-app.post('/forgot-password', forgotPasswordHandler, (req, res) => {
-  
-});
+app.post('/forgot-password', forgotPasswordHandler, (req, res) => {});
 // app.get('/forgot-password', (req, res) => {});
 
-app.get('/reset-password', (req, res) => {});
-app.post('/reset-password', (req, res) => {})
+app.get('/reset-password/:id/:token', resetPasswordHandler, (req, res, next) => {
+  
+});
+app.post('/reset-password/:id/:token', sendNewPassword, (req, res) => {})
 
 export default app;
