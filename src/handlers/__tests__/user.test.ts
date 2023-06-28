5b94e5497954afc7c54d08363ee7c00c2d192af8
lucas neves <casluhc@gmail.com>
2023-06-28 17:41:22 -0300

    First deploy

--- src/handlers/__tests__/user.test.ts
+++ src/handlers/__tests__/user.test.ts
@@ -2,16 +2,8 @@ import app from "../../server";
 import supertest from "supertest";
 
 describe("Sign up user", () => {
-  const payload = {
-    body: {
-      username: '',
-      email: 'lucasneves@gmail',
-      password: '123456'
-    }
-  }
-  const res = { message: "User created successfully" };
   test("should respond with a 201 status code", async () => {
-    const response = await supertest(app).post('/signup').send({
+    await supertest(app).post('/signup').send({
       username: 'glenda gibson',
       email: 'glenda.gibson@example.com',
       password: '123456'
